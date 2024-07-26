00f5fd41edb56fa8d0c329038991bb19324d17bf
passivegoose <boris.mikhail09200@gmail.com>
2024-07-26 19:41:57 +0300

    solved task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld firstHWBean = applicationContext.getBean(HelloWorld.class);
+        HelloWorld secondHWBean = applicationContext.getBean(HelloWorld.class);
+
+        Cat firstCBean = applicationContext.getBean(Cat.class);
+        Cat secondCBean = applicationContext.getBean(Cat.class);
+
+        System.out.println(firstHWBean == secondHWBean);
+        System.out.println(firstCBean == secondCBean);
     }
 }
